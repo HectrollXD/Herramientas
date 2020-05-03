0b6b11551303f9775f278eda42077dc7581751b0
Hector Rodriguez <hector.rmtnez89@gmail.com>
2020-05-03 16:57:27 -0500

    Modificacion

--- JS/AutoCreateDB.js
+++ JS/AutoCreateDB.js
@@ -2,6 +2,8 @@ $(document).ready(main);
 
 function main(){
     $('#AutoCreateDB').click(function(){ window.location.href="SCRIPT/auto_create_database.php"; });
+    $('#ManuallyCreateDB').click(function(){ window.location.href="SQL/database.txt"; });
+    $('#GotoProgram').click(function(){ window.location.href="VIEWS/index.php"; });
     $('#rti').click(function(){ window.location.href="../index.php"; });
     $('#gtphpmyadmin').click(function(){ window.location.href="../SQL/database.txt"; });
 }
--- index.php
+++ index.php
@@ -9,7 +9,13 @@
         <header>
         </header>
         <div>
-            <input type="button" id="AutoCreateDB" value="Auto create database">
+            <div>
+                <input type="button" id="AutoCreateDB" value="Database auto create">
+                <input type="button" id="ManuallyCreateDB" value="Database manually create">
+            </div>
+            <div>
+                <input type="button" id="GotoProgram" value="Go to program">
+            </div>
         </div>
     </body>
     <script src="JS/jQuery.js"></script>
