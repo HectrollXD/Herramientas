f0b157c6ac1ed137b7d0c696de776a9170eb97fd
Hector Rodriguez <hector.rmtnez89@gmail.com>
2020-05-06 00:04:58 -0500

    agregacion del navbar

---
+++ VIEWS/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../CSS/bootstrap/bootstrap.min.css">
+        <title>Prestamos</title>
+    </head>
+    <body>
+        <header>
+            <?php include ("SHARED/header.php"); ?>
+        </header>
+        <div class="container">
+        </div>
+        <footer>
+        </footer>
+    </body>
+    <script src="../JS/jQuery.js"></script>
+    <script src="../JS/prestamos.js"></script>
+    <script src="../JS/bootstrap/bootstrap.bundle.min.js"></script>
+</html>
